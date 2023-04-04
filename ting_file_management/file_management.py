c32b6d152c476e084083547abfeb7642d175f710
thais viana <thaisviana081@outlook.com>
2023-04-04 17:40:49 -0300

    erro 🥺

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+import os
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    f, extensao = os.path.splitext(path_file)
+    if not path_file:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    if extensao != '.txt':
+        print("Formato inválido", file=sys.stderr)
+
+    with open(path_file, 'r', encoding="utf-8") as files:
+        text = files.read().split('\n')
+        print(text)
