c61b72d833439ba6064052dd458cbd3082651b7d
thaisvc <thaisviana081@outlook.com>
2023-04-05 09:27:50 -0300

    2 - Implemente uma função txt_importer dentro do módulo file_management capaz de importar notícias a partir de um arquivo TXT, utilizando "\n" como separador. 💚

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -3,12 +3,13 @@ import os
 
 
 def txt_importer(path_file):
-    f, extensao = os.path.splitext(path_file)
-    if not path_file:
-        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
-    if extensao != '.txt':
-        print("Formato inválido", file=sys.stderr)
+    f, extension = os.path.splitext(path_file)
+    if extension != '.txt':
+        return print("Formato inválido", file=sys.stderr)
 
-    with open(path_file, 'r', encoding="utf-8") as files:
-        text = files.read().split('\n')
-        print(text)
+    if (os.path.isfile(path_file)):
+        with open(path_file, 'r', encoding="utf-8") as files:
+            text = files.read().split("\n")
+        return text
+    else:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
