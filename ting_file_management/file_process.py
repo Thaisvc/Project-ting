65a611e19923ead561e9c6923d024485363182ed
thaisvc <thaisviana081@outlook.com>
2023-04-05 14:20:59 -0300

    3 - Implemente a função process no módulo file_process. Essa função deverá ser capaz de transformar o conteúdo da lista gerada pela função txt_importer em um dicionário que será armazenado dentro da Queue. 🙁

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+
+    lines = txt_importer(path_file)
+
+    file_dic = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+
+    for item in instance._queue:  # lista =  print(q._queue) mostra a lista
+        if item["nome_do_arquivo"] == path_file:
+            return
+
+    instance.enqueue(file_dic)
+
+    print(f"Arquivo {path_file} processado: {file_dic}")
 
 
 def remove(instance):
@@ -8,3 +26,8 @@ def remove(instance):
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+""" if __name__ == "__main__":
+    q = Queue()
+    process("statics/arquivo_teste.txt", q) """
