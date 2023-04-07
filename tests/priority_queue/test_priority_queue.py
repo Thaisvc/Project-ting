a45ecc5e8bb0eef2a640dcdf7562ca0a5edfb99d
thaisvc <thaisviana081@outlook.com>
2023-04-07 09:09:28 -0300

    6 - Implemente os testes para uma classe PriorityQueuecapaz de armazenar arquivos pequenos de forma prioritária 🤯

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,34 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    file = PriorityQueue()
+    quantity_lines = [
+        {"qtd_linhas": 2},
+        {"qtd_linhas": 5},
+        {"qtd_linhas": 4},
+        {"qtd_linhas": 6},
+        {"qtd_linhas": 3}
+    ]
+    priority_files = [
+        {"qtd_linhas": 2},
+        {"qtd_linhas": 3},
+        {"qtd_linhas": 4},
+        {"qtd_linhas": 5},
+        {"qtd_linhas": 6}
+    ]
+
+    for row in quantity_lines:
+        row.enqueue(row)
+
+    for i in range(row.__len__()):
+        assert row.search(i) == priority_files[i]
+
+    for row in priority_files:
+        assert row.dequeue() == row
+
+    with pytest.raises(
+            IndexError,
+            match="Índice Inválido ou Inexistente"):
+        file.search(0)
